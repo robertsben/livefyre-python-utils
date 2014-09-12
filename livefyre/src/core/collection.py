35cb3c87ecb20824a129cad8c43b6d9734d3398f
David Lee <daegun.lee@gmail.com>
2014-09-12 01:31:50 -0700

    updating collection

--- livefyre/src/core/collection.py
+++ livefyre/src/core/collection.py
@@ -11,6 +11,18 @@ except ImportError:
     import json
 
 
+def check_topics(network_urn, topics):
+    if topics:
+        for topic in topics:
+            try:
+                topic_id = topic.get_id()
+                if topic_id.startswith(network_urn) and not topic_id.replace(network_urn, '?', 1).startswith(':site='):
+                    return True
+            except AttributeError:
+                continue # not a topic!
+    return False
+
+
 class Collection(object):
     TYPE = ['reviews', 'sidenotes', 'ratings', 'counting', 'liveblog', 'livechat', 'livecomments']
     
@@ -18,8 +30,11 @@ class Collection(object):
         assert is_valid_full_url(url), 'url must be a full domain. ie. http://livefyre.com'
         assert len(title) <= 255, "title's length should be under 255 char"
         
-        if 'type' in self.options and self.options['type'] not in self.TYPE:
-            raise AssertionError('type is not a recognized type. must be in {0}'.format(self.TYPE))
+        if self.options:
+            if 'type' in self.options and self.options['type'] not in self.TYPE:
+                raise AssertionError('type is not a recognized type. must be in {0}'.format(self.TYPE))
+            if 'topics' in self.options:
+                self.network_issued = check_topics(site.network.get_urn(), self.options['topics'])
 
         self.site = site
         self.article_id = article_id
@@ -29,59 +44,30 @@ class Collection(object):
     
     
     def create_or_update(self):
-        response = self.invoke_collection_api('create')
-        
+        response = self.__invoke_collection_api('create')
         if response.status_code == 200:
             self.collection_id = response.json()['data']['collectionId']
-            return
+            return self
         if response.status_code == 409:
-            response = self.invoke_collection_api('update')
+            response = self.__invoke_collection_api('update')
             if response.status_code == 200:
                 return self
             raise LivefyreException('Error updating Livefyre collection. Status code: ' + response.status_code)
         raise LivefyreException('Error creating Livefyre collection. Status code: ' + response.status_code)
 
-
-    def get_json(self):
-        d = {
-            'articleId': self.article_id,
-            'url': self.url,
-            'title': self.title,
-        }
-        d.update(self.options)
-        
-        return collections.OrderedDict(sorted(d.items()))
-    
-    
-    def get_payload(self):
-        return {
-            'articleId': self.article_id,
-            'collectionMeta': self.build_collection_meta_token(),
-            'checksum': self.build_checksum(),
-        }
-        
-    
-    def invoke_collection_api(self, method):
-        uri = '{0}/api/v3.0/site/{1}/collection/{2}/'.format(Domain.quill(self), self.s_id, method)
-        data = self.get_payload()
-        headers = {'Content-Type': 'application/json', 'Accepts': 'application/json'}
-            
-        response = requests.post(uri, params={'sync':1}, data=json.dumps(data), headers=headers)
-        return response
-    
     
     def build_collection_meta_token(self):
-        j = self.get_json()
+        j = self.__get_json()
         if self.network_issued:
             j['iss'] = self.site.network.getUrn()
         
-        return jwt.encode(j, self.key)
+        return jwt.encode(j, self.site.network.key if self.network_issued else self.key)
     
     
     def build_checksum(self):
-        meta_string = '{{"tags":"{0}","title":"{1}","url":"{2}"}}'.format(self.tags, self.title, self.url)
+        j = self.__get_json()
 
-        return hashlib.md5(meta_string).hexdigest()
+        return hashlib.md5(str(json.dumps(info))).hexdigest()
     
     
     def get_collection_content(self, article_id):
@@ -98,16 +84,37 @@ class Collection(object):
         return None
 
 
-    def get_collection_id(self, article_id):
-        json = self.get_collection_content(article_id)
-        if json:
-            return json['collectionSettings']['collectionId']
-        return None
-    
-    
     def build_livefyre_token(self):
         return self.site.build_livefyre_token()
 
 
     def get_urn(self):
         return self.site.get_urn() + ":collection=" + self.s_id
+    
+
+    def __invoke_collection_api(self, method):
+        uri = '{0}/api/v3.0/site/{1}/collection/{2}/'.format(Domain.quill(self), self.s_id, method)
+        data = self.__get_payload()
+        headers = {'Content-Type': 'application/json', 'Accepts': 'applicatio/json'}
+            
+        response = requests.post(uri, params={'sync':1}, data=json.dumps(data), headers=headers)
+        return response
+    
+    
+    def __get_json(self):
+        d = {
+            'articleId': self.article_id,
+            'url': self.url,
+            'title': self.title,
+        }
+        d.update(self.options)
+        
+        return collections.OrderedDict(sorted(d.items()))
+    
+    
+    def __get_payload(self):
+        return {
+            'articleId': self.article_id,
+            'collectionMeta': self.build_collection_meta_token(),
+            'checksum': self.build_checksum(),
+        }
